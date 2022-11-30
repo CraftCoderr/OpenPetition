441fda829531cb346c55f5cc3bba91104ebc57e6
Nikita Klyshko <n-klyshko@mail.ru>
2022-11-30 13:11:05 +0300

    Closes #8 Implement endpoint for saving signature info

---
+++ src/Entity/Signature.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SignatureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: SignatureRepository::class)]
+class Signature
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: false)]
+    #[Assert\NotBlank]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: false)]
+    #[Assert\NotBlank]
+    private ?string $surname = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $patronymic = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 16777215)]
+    #[Assert\NotBlank]
+    private ?string $signature_writing = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Assert\NotBlank]
+    private ?\DateTimeInterface $signing_date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(?string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getPatronymic(): ?string
+    {
+        return $this->patronymic;
+    }
+
+    public function setPatronymic(?string $patronymic): self
+    {
+        $this->patronymic = $patronymic;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSignatureWriting(): ?string
+    {
+        return $this->signature_writing;
+    }
+
+    public function setSignatureWriting(string $signature_writing): self
+    {
+        $this->signature_writing = $signature_writing;
+
+        return $this;
+    }
+
+    public function getSigningDate(): ?\DateTimeInterface
+    {
+        return $this->signing_date;
+    }
+
+    public function setSigningDate(\DateTimeInterface $signing_date): self
+    {
+        $this->signing_date = $signing_date;
+
+        return $this;
+    }
+}
