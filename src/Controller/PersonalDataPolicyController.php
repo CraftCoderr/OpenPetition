493d22171448e62d204f1ad30b2d07387065d668
Nikita Klyshko <n-klyshko@mail.ru>
2022-12-02 22:27:04 +0300

    Closes #6 Implement 'Personal data policy' page (#24)

---
+++ src/Controller/PersonalDataPolicyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonalDataPolicyController extends AbstractController
+{
+    #[Route('/personal-data-policy', name: 'app_personal_data_policy')]
+    public function index(): Response
+    {
+        return $this->render('personal_data_policy.html.twig', [
+            'sitename' => $this->getParameter('app.sitename'),
+            'siteurl' => $this->getParameter('app.siteurl'),
+            'sitemail' => $this->getParameter('app.sitemail'),
+            'siteowner_who' => $this->getParameter('app.siteowner_who'),
+            'siteowner_by_whom' => $this->getParameter('app.siteowner_by_whom'),
+            'siteowner_birthdate' => $this->getParameter('app.siteowner_birthdate')
+        ]);
+    }
+}
